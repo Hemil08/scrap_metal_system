3d4a4de59d471ed327c245a80e11eb44edbcbc4b
Hemil08 <hemilsorathiya008@gmail.com>
2026-06-22 12:14:12 +0200

    feat: add scrap and inventory reducers to Redux store configuration

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import scrapReducer from './scrapSlice'
+import inventoryReducer from './inventorySlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    scrap: scrapReducer,
+    inventory: inventoryReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
